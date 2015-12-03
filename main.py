c6ebb925271079343ca92ab1b5b267e70c694818
Chelsea <cirizarr@cs.umw.edu>
2015-12-02 23:32:04 -0500

    Added Euclidean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import math
 
 def csvParser(filename):
     thisData = []
@@ -11,3 +12,16 @@ def csvParser(filename):
         else:
             counter += 1
     return thisData
+
+def euclidean(rating1, rating2):#still needs to be changed
+    distance = 0
+    commonRatings = False 
+    for key in rating1:
+        if key in rating2:
+            distance += abs((rating1[key] - rating2[key])**2)
+            commonRatings = True
+    distance1=math.sqrt(distance)
+    if commonRatings:
+        return distance1
+    else:
+        return -1 #Indicates no ratings in common
